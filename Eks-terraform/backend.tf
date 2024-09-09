4e0f557a165e6c72c67c990ab929a97fba40cd3c
Sachin Ayeshmantha <132859045+Sachin20010517@users.noreply.github.com>
2024-09-09 05:43:02 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Sachin-s-DevOps-Projects/chatbot-ui" # Replace with your actual S3 bucket name
+    bucket = "sachin-chatbot-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
